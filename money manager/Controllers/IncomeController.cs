70f47455f33ccb5ebf17ab988d9363982452ceb0
siddhantPaudel <siddhantpaudel777@gmail.com>
2024-07-01 07:16:22 +0545

    created income controller and model

---
+++ money manager/Controllers/IncomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace money_manager.Controllers
+{
+    public class IncomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- money manager/Models/IncomeModel.cs
+++ money manager/Models/IncomeModel.cs
@@ -1,15 +1,12 @@
 ﻿namespace money_manager.Models
 {
-    public class ExpenseModel
+    public class IncomeModel
     {
-        public int ExpenseId { get; set; }
+        public int IncomeId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public double Amount { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
-
-
-
     }
 }
